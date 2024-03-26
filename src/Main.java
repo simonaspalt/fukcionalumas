6926a88c22802f693c1f72b5d92ee7756f6c108b
simonaspalt <simaspa@gmail.com>
2024-03-26 13:10:38 +0200

    trimmed space in front and back

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,22 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println(formatName("naglis"));
         System.out.println(assertEquals(formatName("NAGLIS"), "Naglis"));
-        System.out.println(assertEquals(formatName("nAGLIS"), "Naglis"));
+        System.out.println(assertEquals(formatName("nAGLIS   "), "Naglis"));
+        System.out.println(assertEquals(formatName(" naglis"), "Naglis"));
+        System.out.println(assertEquals(formatName(" nag-lis"), "Nag-lis"));
         System.out.println(assertEquals(formatName("naglis"), "Naglis"));
-
-
+        System.out.println(assertEquals(formatName(" nag lis"), "Nag lis"));
     }
 
-    public static String formatName(String name){
-        return name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase();//suformatuotas Namecase
+    public static String formatName(String name) {
+        name = name.trim();
+        name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();//suformatuotas Namecase
+        return name;
     }
 
-    public static boolean assertEquals(String actual, String expected){
+    public static boolean assertEquals(String actual, String expected) {
         return actual.equals(expected);
     }
-
 
 
 }
