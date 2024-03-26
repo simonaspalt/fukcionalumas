2fa353b4e44b74dc9cd675552fdc667a3c32e160
simonaspalt <simaspa@gmail.com>
2024-03-26 13:57:43 +0200

    xtra tc

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,27 @@ public class Main {
         System.out.println(assertEquals(formatName("nAGLIS   "), "Naglis"));
         System.out.println(assertEquals(formatName(" naglis"), "Naglis"));
         System.out.println(assertEquals(formatName(" nag-lis"), "Nag-lis"));
-        System.out.println(assertEquals(formatName("naglis"), "Naglis"));
         System.out.println(assertEquals(formatName(" nag lis"), "Nag lis"));
+        System.out.println(assertEquals(formatName("n"), "N"));
+        System.out.println(assertEquals(formatName("        "), "name is invalid"));
+        System.out.println(assertEquals(formatName("NAG   LIS"), "Nag lis"));
+        System.out.println(assertEquals(formatName("N AG   LI S"), "N ag li s"));
+        System.out.println(assertEquals(formatName(""), "name is invalid"));
+
+
+
+
+
     }
 
     public static String formatName(String name) {
+        name = name.replaceAll("\\s{2,}", " ");
         name = name.trim();
-        name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();//suformatuotas Namecase
-        return name;
+        if (name.length() == 0){
+            return "name is invalid";
+        }
+        else {name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();//suformatuotas Namecase
+        return name;}
     }
 
     public static boolean assertEquals(String actual, String expected) {
